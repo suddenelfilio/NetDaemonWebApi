2bdb99f05613c2a99b1694f81d2dd984904af930
Filip STAS <filip@visuasoft.be>
2021-01-27 16:06:31 +0100

    added web api endpoint

--- apps/HelloWorld/HelloWorld.cs
+++ apps/HelloWorld/HelloWorld.cs
@@ -1,5 +1,9 @@
 using System;
 using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using NetDaemon.Common.Reactive;
 
 // Use unique namespaces for your apps if you going to share with others to avoid
@@ -9,10 +13,25 @@ namespace HelloWorld
     /// <summary>
     ///     Hello world showcase
     /// </summary>
+
+
     public class HelloWorldApp : NetDaemonRxApp
     {
-        public override void Initialize()
+    
+
+        IWebHost api;
+
+        public override Task InitializeAsync()
         {
+            api = WebHost.CreateDefaultBuilder()
+                .ConfigureServices(svc =>
+                {
+                    svc.AddSingleton<INetDaemonRxApp>(this);
+                })
+                .UseStartup<daemonapp.apps.HelloWorld.StartUp>()
+                .Build();
+            api.RunAsync();
+
             Entity("binary_sensor.mypir").StateChanges
                 .Where(e => e.New?.State == "on")
                 .Subscribe(
@@ -22,7 +41,23 @@ namespace HelloWorld
                         Entity("light.mylight").TurnOn();
                     }
                 );
-                Log("Hello World");
+            Log("Hello World");
+
+            StateChanges.Subscribe(e =>
+            {
+                Console.WriteLine(e);
+            });
+
+            return Task.CompletedTask;
         }
+
+        public override ValueTask DisposeAsync()
+        {
+
+            api.Dispose();
+            return base.DisposeAsync();
+        }
+       
+
     }
 }
